158910f91e5e405b639451c8f6a3d62af269aaad
Ronak Lala <ronaklala2010@gmail.com>
2022-06-06 15:37:24 +0530

    Added timestamp of adding post in backend

--- src/backend/schemas/postSchema.js
+++ src/backend/schemas/postSchema.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 
-const postSchema = mongoose.Schema({
-  username: String,
-  wallet: String,
-  tag: String,
-  caption: String,
-  image: String,
-});
+const postSchema = mongoose.Schema(
+  {
+    username: String,
+    wallet: String,
+    tag: String,
+    caption: String,
+    image: String,
+  },
+  {timestamps: {type: Number, default: new Date().getTime()}}
+);
 
 const Post = mongoose.model('Post', postSchema);
 
